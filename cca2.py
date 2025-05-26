08c87bebcd48067d24562de1541e72c2c8a5d3e0
Ramon Fernandez <ramonfernandez@Michaels-MacBook-Pro.local>
2025-05-25 20:38:05 -0400

    added complete comments explaining cca2.py file

--- cca2.py
+++ cca2.py
@@ -5,17 +5,17 @@ import matplotlib.patches as patches
 import localization
 
 # this gets all the connected regions and groups them together
-label_image = measure.label(localization.binary_car_image)
+label_image = measure.label(localization.binary_car_image) #label the connected regions in binary 
 
 # getting the maximum width, height and minimum width and height that a license plate can be
-plate_dimensions = (0.08*label_image.shape[0], 0.2*label_image.shape[0], 0.15*label_image.shape[1], 0.4*label_image.shape[1])#making a tuple called plate_dimensions with min_height, max_height, min_width, max_width
+plate_dimensions = (0.08*label_image.shape[0], 0.2*label_image.shape[0], 0.15*label_image.shape[1], 0.4*label_image.shape[1])#making a tuple called plate_dimensions with min_height, max_height, min_width, max_width. label_image.shape is a tuple with the height and width of the image. So, label_image[0] is the height in pixels, and label_image[1] is the width in pixels.
 min_height, max_height, min_width, max_width = plate_dimensions#"unpacking" the tuple into the variables
 plate_objects_cordinates = []
 plate_like_objects = []
-fig, (ax1) = plt.subplots(1)
-ax1.imshow(localization.gray_car_image, cmap="gray");
+fig, (ax1) = plt.subplots(1) #create a figure and a subplot (ax1) for displaying the image. The 1 next to subplots means there is one row and one column of one subplot in the figure.
+ax1.imshow(localization.gray_car_image, cmap="gray"); #Display the grayscale car image on the subplot using a gray colormap
 
-# regionprops creates a list of properties of all the labelled regions
+#regionprops creates a list of properties of all the labelled regions. region is an object -specifically an instance of the RegionProperties class. You access the properties as attributes of the object (.area, .bbox, .centroid etc)
 for region in regionprops(label_image):
     if region.area < 50:
         #if the region is so small then it's likely not a license plate
@@ -27,13 +27,13 @@ for region in regionprops(label_image):
     region_width = max_col - min_col
     # ensuring that the region identified satisfies the condition of a typical license plate
     if region_height >= min_height and region_height <= max_height and region_width >= min_width and region_width <= max_width and region_width > region_height:
-        plate_like_objects.append(localization.binary_car_image[min_row:max_row, min_col:max_col])
+        plate_like_objects.append(localization.binary_car_image[min_row:max_row, min_col:max_col]) #localization.binary_car_image is the black and white image of the car. This uses slicing to extract a rectangular region from the image. These "cropped" images are stored in the plate_like_objects list.
 
-        plate_objects_cordinates.append((min_row, min_col, max_row, max_col))
+        plate_objects_cordinates.append((min_row, min_col, max_row, max_col)) #for each plate_like_object, it stores the bounding box coordinates of it. min_row is the topmost row of the region. min_col is the leftmost column of the region. max_row is the bottommost row of the region. max_col is the rightmost row of the region. In other words it stores the pixel coordinates for the highest, lowest, most left and most right points.
 
-        rectBorder = patches.Rectangle((min_col, min_row), max_col-min_col, max_row-min_row, edgecolor="red", linewidth=2, fill=False)
-        ax1.add_patch(rectBorder)
-    # let's draw a red rectangle over those regions
+        rectBorder = patches.Rectangle((min_col, min_row), max_col-min_col, max_row-min_row, edgecolor="red", linewidth=2, fill=False)#creates a red rectangle surrounding the region
+        ax1.add_patch(rectBorder)#adds the rectangle to the subplot
+
 
 plt.show()
 
